bd10f001ec0a8d2af436c49fd6fb4a185f2f1e42
msmpunk <msm0102712@gmail.com>
2020-06-18 04:08:33 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  }
+];
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{ useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
