ee3182a190c520489af77d5d402cf8cadc7d656a
msmpunk <msm0102712@gmail.com>
2020-06-18 04:26:36 -0500

    + Update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 
 export const routes: Routes = [
   {
     path: '',
     component: LoginComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 
